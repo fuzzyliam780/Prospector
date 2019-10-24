f0c7e574fba3b28b613a4dbdd3c2167535281647
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-10-23 22:38:04 -0400

    everything up to adding chain scoring

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -15,8 +15,107 @@ public class Card : MonoBehaviour {
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+    public SpriteRenderer[] spriteRenderers;
 
-	public bool faceUp {
+
+
+    void Start()
+    {
+
+        SetSortOrder(0);  // Ensures that the card starts properly depth sorted
+
+    }
+    public void PopulateSpriteRenderers()
+    {
+
+        // If spriteRenderers is null or empty
+
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+
+            // Get SpriteRenderer Components of this GameObject and its children
+
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+
+        }
+
+    }
+    public void SetSortingLayerName(string tSLN)
+    {
+
+        PopulateSpriteRenderers();
+
+
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+
+            tSR.sortingLayerName = tSLN;
+
+        }
+
+    }
+    public void SetSortOrder(int sOrd)
+    {                                     // a
+
+        PopulateSpriteRenderers();
+
+
+
+        // Iterate through all the spriteRenderers as tSR
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+
+            if (tSR.gameObject == this.gameObject)
+            {
+
+                // If the gameObject is this.gameObject, it's the background
+
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+
+                continue; // And continue to the next iteration of the loop
+
+
+
+            }
+
+            // Each of the children of this GameObject are named
+
+            // switch based on the names
+
+            switch (tSR.gameObject.name)
+            {
+
+                case "back": // if the name is "back"
+
+                    // Set it to the highest layer to cover the other sprites
+
+                    tSR.sortingOrder = sOrd + 2;
+
+                    break;
+
+
+
+
+                case "face":  // if the name is "face"
+
+                default:      //  or if it's anything else
+
+                    // Set it to the middle layer to be above the background          
+
+                    tSR.sortingOrder = sOrd + 1;
+
+                    break;
+
+            }
+
+        }
+
+    }
+
+
+    public bool faceUp {
 		get {
 			return (!back.activeSelf);
 		}
@@ -24,18 +123,19 @@ public class Card : MonoBehaviour {
 		set {
 			back.SetActive(!value);
 		}
-	}
+    }
+    virtual public void OnMouseUpAsButton()
+    {
 
+        print(name); // When clicked, this outputs the card name
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 } // class Card
 
 [System.Serializable]
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -12,17 +12,444 @@ public class Prospector : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
 
+    public TextAsset layoutXML;
 
-	[Header("Set Dynamically")]
+    public float xOffset = 3;
+
+    public float yOffset = -2.5f;
+
+    public Vector3 layoutCenter;
+
+
+    [Header("Set Dynamically")]
 	public Deck					deck;
 
-	void Awake(){
+    public Layout layout;
+
+    public List<CardProspector> drawPile;
+
+    public Transform layoutAnchor;
+
+    public CardProspector target;
+
+    public List<CardProspector> tableau;
+
+    public List<CardProspector> discardPile;
+
+    void Awake(){
 		S = this;
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
-	}
+    void Start()
+    {
+        deck = GetComponent<Deck>();
+
+        deck.InitDeck(deckXML.text);
+
+        Deck.Shuffle(ref deck.cards);
+
+        //   Card c;
+
+        //   for (int cNum = 0; cNum < deck.cards.Count; cNum++)
+        //   {                    // b
+
+        //       c = deck.cards[cNum];
+
+        //       c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        //   }
+
+        layout = GetComponent<Layout>();  // Get the Layout component
+
+        layout.ReadLayout(layoutXML.text);
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+
+        LayoutGame();
+
+    }
+
+    CardProspector Draw()
+    {
+
+        CardProspector cd = drawPile[0];
+
+        drawPile.RemoveAt(0);
+
+        return (cd);
+
+    }
+
+    void LayoutGame()
+    {
+
+        if (layoutAnchor == null)
+        {
+
+            GameObject tGO = new GameObject("_LayoutAnchor");
+
+            layoutAnchor = tGO.transform;
+
+            layoutAnchor.transform.position = layoutCenter;
+
+        }
+
+        CardProspector cp;
+
+        foreach (SlotDef tSD in layout.slotDefs)
+        {
+
+            cp = Draw();
+
+            cp.faceUp = tSD.faceUp;
+
+            cp.transform.parent = layoutAnchor;
+
+            cp.transform.localPosition = new Vector3(
+
+                layout.multiplier.x * tSD.x,
+
+                layout.multiplier.y * tSD.y,
+
+                -tSD.layerID);
+
+            cp.layoutID = tSD.id;
+
+            cp.slotDef = tSD;
+
+            cp.state = eCardState.tableau;
+
+            cp.SetSortingLayerName(tSD.layerName);
+
+
+
+
+
+
+            tableau.Add(cp);
+
+        }
+
+        foreach (CardProspector tCP in tableau)
+        {
+
+            foreach (int hid in tCP.slotDef.hiddenBy)
+            {
+
+                cp = FindCardByLayoutID(hid);
+
+                tCP.hiddenBy.Add(cp);
+
+            }
+
+        }
+
+        MoveToTarget(Draw());
+
+        UpdateDrawPile();
+
+    }
+
+    CardProspector FindCardByLayoutID(int layoutID)
+    {
+
+        foreach (CardProspector tCP in tableau)
+        {
+
+            if (tCP.layoutID == layoutID)
+            {
+
+                return (tCP);
+
+            }
+
+        }
+
+        return (null);
+
+    }
+
+    void SetTableauFaces()
+    {
+
+        foreach (CardProspector cd in tableau)
+        {
+
+            bool faceUp = true;
+
+            foreach (CardProspector cover in cd.hiddenBy)
+            {
+
+                if (cover.state == eCardState.tableau)
+                {
+
+                    faceUp = false;
+
+                }
+
+            }
+
+            cd.faceUp = faceUp;
+
+        }
+
+    }
+
+    void MoveToDiscard(CardProspector cd)
+    {
+
+
+        cd.state = eCardState.discard;
+
+        discardPile.Add(cd);
+
+        cd.transform.parent = layoutAnchor;
+
+
+
+        cd.transform.localPosition = new Vector3(
+
+            layout.multiplier.x * layout.discardPile.x,
+
+            layout.multiplier.y * layout.discardPile.y,
+
+            -layout.discardPile.layerID + 0.5f);
+
+        cd.faceUp = true;
+
+        cd.SetSortingLayerName(layout.discardPile.layerName);
+
+        cd.SetSortOrder(-100 + discardPile.Count);
+
+    }
+
+    void MoveToTarget(CardProspector cd)
+    {
+
+
+        if (target != null) MoveToDiscard(target);
+
+        target = cd;
+
+        cd.state = eCardState.target;
+
+        cd.transform.parent = layoutAnchor;
+
+        cd.transform.localPosition = new Vector3(
+
+            layout.multiplier.x * layout.discardPile.x,
+
+            layout.multiplier.y * layout.discardPile.y,
+
+            -layout.discardPile.layerID);
+
+
+
+
+
+        cd.faceUp = true;
+
+        cd.SetSortingLayerName(layout.discardPile.layerName);
+
+        cd.SetSortOrder(0);
+
+    }
+
+    void UpdateDrawPile()
+    {
+
+        CardProspector cd;
+
+        for (int i = 0; i < drawPile.Count; i++)
+        {
+
+            cd = drawPile[i];
+
+            cd.transform.parent = layoutAnchor;
+
+
+
+            Vector2 dpStagger = layout.drawPile.stagger;
+
+            cd.transform.localPosition = new Vector3(
+
+                layout.multiplier.x * (layout.drawPile.x + i * dpStagger.x),
+
+                layout.multiplier.y * (layout.drawPile.y + i * dpStagger.y),
+
+                -layout.drawPile.layerID + 0.1f * i);
+
+
+
+
+            cd.faceUp = false;
+
+            cd.state = eCardState.drawpile;
+
+            cd.SetSortingLayerName(layout.drawPile.layerName);
+
+            cd.SetSortOrder(-10 * i);
+
+        }
+
+    }
+
+    public void CardClicked(CardProspector cd)
+    {
+
+        switch (cd.state)
+        {
+
+            case eCardState.target:
+
+                break;
+
+
+
+            case eCardState.drawpile:
+
+                MoveToDiscard(target);
+
+                MoveToTarget(Draw());
+
+                UpdateDrawPile();
+
+                break;
+
+
+
+
+
+            case eCardState.tableau:
+
+                bool validMatch = true;
+
+                if (!cd.faceUp)
+                {
+
+                    validMatch = false;
+
+                }
+
+                if (!AdjacentRank(cd, target))
+                {
+
+                    validMatch = false;
+
+                }
+
+                if (!validMatch) return;
+
+                tableau.Remove(cd);
+
+                MoveToTarget(cd);
+
+                SetTableauFaces();
+
+                break;
+
+        }
+
+        CheckForGameOver();
+
+    }
+    void CheckForGameOver()
+    {
+
+        if (tableau.Count == 0)
+        {
+
+            GameOver(true);
+
+            return;
+
+        }
+
+
+        if (drawPile.Count > 0)
+        {
+
+            return;
+
+        }
+
+
+        foreach (CardProspector cd in tableau)
+        {
+
+            if (AdjacentRank(cd, target))
+            {
+
+                return;
+
+            }
+
+        }
+
+
+        GameOver(false);
+
+    }
+    void GameOver(bool won)
+    {
+
+        if (won)
+        {
+
+            print("Game Over. You won! :)");
+
+        }
+        else
+        {
+
+            print("Game Over. You Lost. :(");
+
+        }
+
+        SceneManager.LoadScene("__Prospector_Scene_0");
+
+    }
+
+    public bool AdjacentRank(CardProspector c0, CardProspector c1)
+    {
+
+        if (!c0.faceUp || !c1.faceUp) return (false);
+
+
+        if (Mathf.Abs(c0.rank - c1.rank) == 1)
+        {
+
+            return (true);
+
+        }
+
+        if (c0.rank == 1 && c1.rank == 13) return (true);
+
+        if (c0.rank == 13 && c1.rank == 1) return (true);
+
+
+
+        return (false);
+
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+
+        List<CardProspector> lCP = new List<CardProspector>();
+
+        CardProspector tCP;
+
+        foreach (Card tCD in lCD)
+        {
+
+            tCP = tCD as CardProspector;
+
+            lCP.Add(tCP);
+
+        }
+
+        return (lCP);
+
+    }
 
 }
