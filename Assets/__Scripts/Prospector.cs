b5c76efeafa2ddce22a701b21da36728ab20678d
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-10-23 23:07:17 -0400

    Complete
    Bug game not spawning

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -20,6 +20,15 @@ public class Prospector : MonoBehaviour {
 
     public Vector3 layoutCenter;
 
+    public Vector2 fsPosMid = new Vector2(0.5f, 0.90f);
+
+    public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
+
+    public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
+
+    public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
+
+
 
     [Header("Set Dynamically")]
 	public Deck					deck;
@@ -36,12 +45,16 @@ public class Prospector : MonoBehaviour {
 
     public List<CardProspector> discardPile;
 
+    public FloatingScore fsRun;
+
     void Awake(){
 		S = this;
 	}
 
     void Start()
     {
+        Scoreboard.S.score = ScoreManager.SCORE;
+
         deck = GetComponent<Deck>();
 
         deck.InitDeck(deckXML.text);
@@ -58,7 +71,7 @@ public class Prospector : MonoBehaviour {
         //       c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
         //   }
 
-        layout = GetComponent<Layout>();  // Get the Layout component
+        layout = GetComponent<Layout>();
 
         layout.ReadLayout(layoutXML.text);
 
@@ -315,6 +328,8 @@ public class Prospector : MonoBehaviour {
 
                 ScoreManager.EVENT(eScoreEvent.draw);
 
+                FloatingScoreHandler(eScoreEvent.draw);
+
                 break;
 
 
@@ -348,6 +363,8 @@ public class Prospector : MonoBehaviour {
                 SetTableauFaces();
 
                 ScoreManager.EVENT(eScoreEvent.mine);
+
+                FloatingScoreHandler(eScoreEvent.mine);
 
                 break;
 
@@ -401,11 +418,15 @@ public class Prospector : MonoBehaviour {
 
             ScoreManager.EVENT(eScoreEvent.gameWin);
 
+            FloatingScoreHandler(eScoreEvent.gameWin);
+
         }
         else
         {
 
             ScoreManager.EVENT(eScoreEvent.gameLoss);
+
+            FloatingScoreHandler(eScoreEvent.gameLoss);
 
         }
 
@@ -435,6 +456,89 @@ public class Prospector : MonoBehaviour {
         return (false);
 
     }
+
+    void FloatingScoreHandler(eScoreEvent evt)
+    {
+
+        List<Vector2> fsPts;
+
+        switch (evt)
+        {
+
+            case eScoreEvent.draw:
+
+            case eScoreEvent.gameWin:
+
+            case eScoreEvent.gameLoss:
+
+                if (fsRun != null)
+                {
+
+                    fsPts = new List<Vector2>();
+
+                    fsPts.Add(fsPosRun);
+
+                    fsPts.Add(fsPosMid2);
+
+                    fsPts.Add(fsPosEnd);
+
+                    fsRun.reportFinishTo = Scoreboard.S.gameObject;
+
+                    fsRun.Init(fsPts, 0, 1);
+
+                    fsRun.fontSizes = new List<float>(new float[] { 28, 36, 4 });
+
+                    fsRun = null;
+
+                }
+
+                break;
+
+
+
+            case eScoreEvent.mine:
+
+                FloatingScore fs;
+
+                Vector2 p0 = Input.mousePosition;
+
+                p0.x /= Screen.width;
+
+                p0.y /= Screen.height;
+
+                fsPts = new List<Vector2>();
+
+                fsPts.Add(p0);
+
+                fsPts.Add(fsPosMid);
+
+                fsPts.Add(fsPosRun);
+
+                fs = Scoreboard.S.CreateFloatingScore(ScoreManager.CHAIN, fsPts);
+
+                fs.fontSizes = new List<float>(new float[] { 4, 50, 28 });
+
+                if (fsRun == null)
+                {
+
+                    fsRun = fs;
+
+                    fsRun.reportFinishTo = null;
+
+                }
+                else
+                {
+
+                    fs.reportFinishTo = fsRun.gameObject;
+
+                }
+
+                break;
+
+        }
+
+    }
+
 
     List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
     {
